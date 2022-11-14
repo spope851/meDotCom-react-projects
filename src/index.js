4b7f5911d46a653c6f9df09a2505e1aaf133c887
spope <spope851@gmail.com>
2022-11-13 21:40:25 -0700

    import cube package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { CubeSolver } from '3x3-cube';
 
 const root = ReactDOM.createRoot(document.getElementById('react-cube-solver'));
 root.render(
   <React.StrictMode>
-    <div>hello</div>
+    <CubeSolver />
   </React.StrictMode>
 );
