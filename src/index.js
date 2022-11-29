31aeb811dcc4e7adff381ebf7060d3f2ddc8fb0c
spope <spope851@gmail.com>
2022-11-28 19:41:21 -0700

    update cube solver version and integrate with new api endpoint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CubeSolver } from '3x3-cube';
+import { CubeSolver, getFacelets } from '3x3-cube';
 
 const root = ReactDOM.createRoot(document.getElementById('react-cube-solver'));
 root.render(
   <React.StrictMode>
-    <CubeSolver />
+    <CubeSolver
+      solve={
+        async () => await
+          fetch(`api/solveCube?facelets=${getFacelets()}`)
+            .then(res => res.json())
+            .then(data => data.join(' ')
+          )
+      } />
   </React.StrictMode>
 );
